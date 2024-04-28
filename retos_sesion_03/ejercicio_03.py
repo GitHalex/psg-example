68df4eb17a2c1dec1c9ec6d6620a5ebad90255cb
GitHalex <calex1731@gmail.com>
2024-04-28 15:07:34 -0400

    ejercicio 3 de la sesion 3 terinado

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,14 @@
+print("""
+  COMO BARRER UNA HABITACION
+      
+      1. buscar y encontrar una escoba
+      2. buscar y encontrar un basurero
+      3. una bolsa para depositar la basura
+  """)
+print("agarramos la escoba y nos ponemos a barrer de un rincon de la habitacion,")
+print("llevar la basura al medio del cuarto de los cuatro rincones de la ") 
+
+print("habitacion. una vez terminado recoger la basura montonada con la ayuda de la ")
+print("escoba empujar la basura al basurero para luego depositarlo a la bolsa y")
+print("depositar la bolsa en un lugar alejado de la habitacion para luego ir a ")
+print("depositarlo al momento de que pase el carro basurero de la ciudad")
