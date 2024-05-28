8764d5e9c55f1e130a2bac6e7b34e53406638a1d
GitHalex <calex1731@gmail.com>
2024-05-28 17:09:48 -0400

    Sesion 4 mi primer notebook

---
+++ sesion05.py
@@ -0,0 +1,13 @@
+print(type(1))
+# Entero con 60 dígitos
+variable_3 = 123456789012345678901234567890123456789012345678901234567890
+print (variable_3)
+print ( type (variable_3) )
+
+# Valor 0.5 Flotante
+print (0.5)
+print ( type (0.5) )
+
+minutos = 300 #minutos
+minutosA_segundos = 3600//60 + minutos
+print(f"HORA: {minutosA_segundos//60}")
