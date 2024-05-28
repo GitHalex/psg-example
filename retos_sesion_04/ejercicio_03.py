e599f53d4aa86760b4cbe9bf7a336e2392185a3c
GitHalex <calex1731@gmail.com>
2024-05-28 17:40:48 -0400

    ejercicio 3 juego de mesa 3 en raya

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,9 @@
+#Reglas del juego Tres en raya
+print("Reglas tres en raya")
+print("1. El juego se juega en un tablero de 3x3")
+print("2. Los jugadores eligen un simbolo para jugar")
+print("3. Inicia el jugador X")
+print("4. Continua el jugadoro 'O' y asi sucesivamente")
+print("5. No se puede jugar en una casilla ocupada")
+print("6. Gana el jugador que logre aliniear tres simbolos en linea horisontal, vertical o diagonal")
+print("7. Si no hay más casillas disponibles y no hay ganador es emppate")
